1e7a8bc8a66d8177d29bea1b17ff4be228b9538c
marvinlaisney04 <laisneymarvin@gmail.com>
2022-04-13 16:19:09 +0200

    first commit

---
+++ src/main/java/com/hopital/architecturems/ArchitecturemsApplication.java
@@ -0,0 +1,13 @@
+package com.hopital.architecturems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitecturemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hopital/architecturems/ArchitecturemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopital.architecturems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
